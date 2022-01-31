d42bfaaed72985745507c484c168381b22e0414c
zaktsy <ibudenov2@gmal.com>
2022-01-31 20:27:26 +0500

    added dockpanel

---
+++ Infrastructure/behaviors/DragWindow.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace recipe.Infrastructure.behaviors
+{
+    public class DragWindow: Behavior<DockPanel>
+    {
+        protected override void OnAttached() => AssociatedObject.MouseDown += OnMouseDown;
+
+        protected override void OnDetaching() => AssociatedObject.MouseUp -= OnMouseDown;
+
+        private void OnMouseDown(object Sender, RoutedEventArgs E)
+        {
+            var window = Application.Current.MainWindow;
+            window.DragMove();
+        }
+    }
+}
