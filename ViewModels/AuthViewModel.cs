f37591e3a4fa0c89c17232d886e40ba925280d4a
zaktsy <ibudenov2@gmal.com>
2022-02-10 16:57:52 +0500

    конец окна авторизации

--- ViewModels/AuthViewModel.cs
+++ ViewModels/AuthViewModel.cs
@@ -12,11 +12,13 @@ namespace recipe.ViewModels
     internal class AuthViewModel : BaseViewModel
     {
         recipesdbContext db;
+
         private User loginedUser;
         private string email;
         private string password;
         public bool userLogin = false;
         private string message = "";
+
         public User LoginedUser
         {
             get { return loginedUser; }
@@ -26,6 +28,7 @@ namespace recipe.ViewModels
                 if (loginedUser == null) { userLogin = false; }
             }
         }
+
         public string Email { get { return email; } set { email = value; OnPropertyChanged("Email"); } }
         public string Password { get { return password; } set { password = value; OnPropertyChanged("Password"); } }
         public bool UserLogin { get { return userLogin; } set { userLogin = value; OnPropertyChanged("UserLogin"); } }
@@ -45,14 +48,13 @@ namespace recipe.ViewModels
                     (checkUserCommand = new LambdaCommand(obj =>
                     {
                         User user = db.Users.Where(u => u.Email == Email).FirstOrDefault();
-                        if (user == null) { Message = "такого пользователя не существует"; }
-                        else if (user.Password != Password) { Message = "Вы ввели неверный пароль"; }
-                        else if (user.Admin != true) { Message = "Вы не являетесь администратором"; }
+                        if (user == null) { Message = "Пользователь не найден"; }
+                        else if (user.Password != Password) { Message = "Неверный пароль"; }
+                        else if (user.Admin != true) { Message = "Вы не администратор"; }
                         else
                         {
                             LoginedUser = user;
                             userLogin = true;
-                            Message = "С возвращением, " + user.Name;
                             Application.Current.MainWindow.Close();
                         }
                         
