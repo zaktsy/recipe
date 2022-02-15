48fe09d8b17ca6e9ed39bc705e8a02e8913f41c1
zaktsy <ibudenov2@gmal.com>
2022-02-15 18:58:29 +0500

    new design for YesNowWindow

--- Infrastructure/behaviors/DragWindow.cs
+++ Infrastructure/behaviors/DragWindow.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.Xaml.Behaviors;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace recipe.Infrastructure.behaviors
 {
-    public class DragWindow: Behavior<DockPanel>
+    public class DragWindow: Behavior<UIElement>
     {
         protected override void OnAttached() => AssociatedObject.MouseDown += OnMouseDown;
 
         protected override void OnDetaching() => AssociatedObject.MouseUp -= OnMouseDown;
 
-        private void OnMouseDown(object Sender, RoutedEventArgs E)
-        {
-            var window = Application.Current.MainWindow;
-            window.DragMove();
+        private void OnMouseDown(object Sender, MouseEventArgs E)
+        {   
+            if (E.LeftButton == MouseButtonState.Pressed)
+            {
+                var window = Application.Current.MainWindow;
+                window.DragMove();
+            }
+            
         }
     }
 }
