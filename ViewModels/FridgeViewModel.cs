3a60204cac444a12fdda8c4e88e624088728e968
zaktsy <ibudenov2@gmal.com>
2022-02-16 14:43:16 +0500

    add delete from fridge command

--- ViewModels/FridgeViewModel.cs
+++ ViewModels/FridgeViewModel.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using recipe.Infrastructure;
+using recipe.Infrastructure.dialogs.DialogService;
+using recipe.Infrastructure.dialogs.DialogYesNo;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace recipe.ViewModels
 {
@@ -34,7 +38,39 @@ namespace recipe.ViewModels
             for(int i = 0; i< Products.Count; i++)
             {
                 Products[i].Product = db.Products.Where(u => u.Id == Products[i].Productid).FirstOrDefault();
+                Products[i].Measure = db.Measures.Where(u => u.Id == Products[i].Measureid).FirstOrDefault();
             }
         }
+
+        #region commands
+        
+
+        private LambdaCommand delFridgeCommand;
+        public LambdaCommand DelFridgeCommand
+        {
+            get
+            {
+                return delFridgeCommand ??
+                    (delFridgeCommand = new LambdaCommand(obj =>
+                    {
+                        DialogViewModelBase vm = new DialogYesNoViewModel("Удалить продукт из холодильника пользователя??");
+                        DialogResult result = DialogService.OpenDialog(vm, obj as Window);
+                        if (result == DialogResult.Yes)
+                        {
+                            var uid = SelectedProduct.Userid;
+                            var pid = SelectedProduct.Productid;
+                            Fridge fr = (from fridge in db.Fridges
+                                         where fridge.Userid == uid && fridge.Productid == pid
+                                         select fridge).FirstOrDefault();
+                            db.Fridges.Remove(fr);
+                            db.SaveChanges();
+                            Products.Remove(SelectedProduct);
+                            SelectedProduct = null;
+                        }
+                    },
+                    (obj) => SelectedProduct != null));
+            }
+        }
+        #endregion
     }
 }
